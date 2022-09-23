c62362e9428656d97045fe1a70077f2e4172074b
Christian Scott <christian.s@canva.com>
2022-09-23 14:05:24 +1000

    add run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+echo_cmd_and_run() {
+    echo '>' "$@" 1>&2
+    "$@"
+}
+
+main() {
+    local base_dir="$1"
+    local tsconfigs_file='./out/tsconfigs.txt'
+	echo_cmd_and_run fd tsconfig.json --base-directory "${base_dir}" > "${tsconfigs_file}"
+	echo_cmd_and_run ./node_modules/.bin/tsc --sourcemap --outDir out
+	echo_cmd_and_run node --enable-source-maps out/index.js --tsconfigs "${tsconfigs_file}" --base-dir "${base_dir}"
+}
+
+main "$@"
