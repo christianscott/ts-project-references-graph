fc127030116b1358769ee63625a2ffb665f190d5
Christian Scott <christian.s@canva.com>
2022-09-23 14:43:08 +1000

    mkdir out/

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ echo_cmd_and_run() {
 main() {
     local base_dir="$1"
     local tsconfigs_file='./out/tsconfigs.txt'
+    mkdir -p out/
+    echo_cmd_and_run pnpm install --use-stderr --silent
 	echo_cmd_and_run fd tsconfig.json --base-directory "${base_dir}" > "${tsconfigs_file}"
 	echo_cmd_and_run ./node_modules/.bin/tsc --sourcemap --outDir out
 	echo_cmd_and_run node --enable-source-maps out/index.js --tsconfigs "${tsconfigs_file}" --base-dir "${base_dir}"
